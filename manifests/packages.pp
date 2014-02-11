b0cf7b70d326328fd2d7b7dc60f678940ec1f0e2
Michael Rennecke <michael.rennecke@gmail.com>
2014-02-11 09:56:16 +0100

    add sysstat package

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,6 +1,6 @@
 class common::packages {
 
-    $packages = [ 'htop', 'vim-puppet', 'puppet', 'puppet-common', 'ethtool', 'zsh', 'git', 'tmux', 'yakuake', 'nodejs', ]
+    $packages = [ 'htop', 'vim-puppet', 'puppet', 'puppet-common', 'ethtool', 'zsh', 'git', 'tmux', 'yakuake', 'sysstat', ]
 
     ensure_packages($packages)
 
