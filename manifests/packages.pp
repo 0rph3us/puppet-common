d01b4e86f59e1a2ca8376d34cc976067b25f96ab
Michael Rennecke <michael.rennecke@gmail.com>
2014-02-19 13:57:44 +0100

    add i3 keyring package

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,6 +1,6 @@
 class common::packages {
 
-    $packages = [ 'htop', 'vim-puppet', 'puppet', 'puppet-common', 'ethtool', 'zsh', 'git', 'tmux', 'yakuake', 'sysstat', ]
+    $packages = [ 'htop', 'vim-puppet', 'puppet', 'puppet-common', 'ethtool', 'zsh', 'git', 'tmux', 'yakuake', 'sysstat', 'sur5r-keyring']
 
     ensure_packages($packages)
 
