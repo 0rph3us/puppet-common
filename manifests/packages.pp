b4ace23fdea83847317f3dfb274ceda9e166e79b
Michael Rennecke <michael.rennecke@gmail.com>
2014-02-19 14:23:28 +0100

    install latest version of a package

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -2,7 +2,7 @@ class common::packages {
 
     $packages = [ 'htop', 'vim-puppet', 'puppet', 'puppet-common', 'ethtool', 'zsh', 'git', 'tmux', 'yakuake', 'sysstat', 'sur5r-keyring']
 
-    ensure_packages($packages)
+    ensure_resource ('package', $packages, {'ensure' => 'latest'})
 
     class { 'common::packages::config':
         require => Class['common::packages'],
